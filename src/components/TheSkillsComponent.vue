4de2fa134e49bd6ab3723cd35c33d99a48d955d3
Marc Pont <mpont91@gmail.com>
2022-07-06 10:36:38 +0200

    WIP draft skills

---
+++ src/components/TheSkillsComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <p>Backend developer roadmap</p>
+  <p>Languages:</p>
+  <p>PHP +5y exp</p>
+  <p>JS +1y exp (not professionally)</p>
+  <p>Java +1y exp (not professionally)</p>
+
+  <p>Webservers</p>
+  <p>Nginx +5y</p>
+  <p>Apache +5y</p>
+
+  <p>Containerization</p>
+  <p>Docker +5y</p>
+  <p>Kubernetes +1y</p>
+
+  <p>FullStack development</p>
+  <p>Git +5y exp</p>
+  <p>hosting services:</p>
+  <p>Github +5y exp</p>
+  <p>bitbucket +5y exp</p>
+  <p>gitlab 1y</p>
+
+  <p>Relational databases</p>
+  <p>Postgresql +3y</p>
+  <p>MySql +5y</p>
+
+  <p>No SQL databases</p>
+  <p>Redis +3y</p>
+  <p>MongoDB +1y no prof</p>
+
+  <p>Frontend</p>
+  <p>Languages</p>
+  <p>HTML +10y</p>
+  <p>CSS +10y</p>
+  <p>JS +5y</p>
+
+  <p>frameworks</p>
+  <p>Vue +5y</p>
+  <p>React 1y no prof</p>
+
+  <p>css frameworks</p>
+  <p>Quasar +2y</p>
+  <p>Vuetify +2y</p>
+  <p>Element UI +1y</p>
+
+  <p>Testing</p>
+  <p>Cypress 2y</p>
+  <p>Jest 2y</p>
+  <p>Vitest 2y</p>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
